ca3f05a10a59cde0ea38bff442a9dc8c15cbee2a
Jussi Hailu <jussihailu1@gmail.com>
2022-06-18 03:06:04 +0200

    removed auth

--- post-service/src/main/kotlin/com/primefootball/postservice/PostServiceApplication.kt
+++ post-service/src/main/kotlin/com/primefootball/postservice/PostServiceApplication.kt
@@ -14,21 +14,5 @@ import java.io.IOException
 class PostServiceApplication
 
 fun main(args: Array<String>) {
-    val jsonPath = "service-account-file.json"
-
-    @Throws(IOException::class)
-    fun authExplicit() {
-        val credentials: GoogleCredentials = GoogleCredentials.fromStream(FileInputStream(jsonPath))
-            .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"))
-
-        val options = FirebaseOptions.builder()
-            .setCredentials(credentials)
-            .setDatabaseUrl("https://prime-football-default-rtdb.europe-west1.firebasedatabase.app/")
-            .build()
-
-        FirebaseApp.initializeApp(options)
-    }
-
     runApplication<PostServiceApplication>(*args)
-    authExplicit()
 }
