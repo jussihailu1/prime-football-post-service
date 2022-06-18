f6fa3bec7c133065d0610c46d79eac1d7a9ad36c
Jussi Hailu <jussihailu1@gmail.com>
2022-06-18 17:28:17 +0200

    it works

--- post-service/src/main/kotlin/com/primefootball/postservice/controllers/RootController.kt
+++ post-service/src/main/kotlin/com/primefootball/postservice/controllers/RootController.kt
@@ -1,10 +1,8 @@
 package com.primefootball.postservice.controllers
 
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.*
 
 @RestController
 @RequestMapping("/hello-posts")
